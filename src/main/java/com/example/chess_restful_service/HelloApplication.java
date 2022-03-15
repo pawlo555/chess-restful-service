8bff0cac5f322ba9d6b91dbfa2ce66206b6e1c95
pawlo555 <spawel555@outlook.com>
2022-03-15 11:51:54 +0100

    Initial commit

---
+++ src/main/java/com/example/chess_restful_service/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.chess_restful_service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
